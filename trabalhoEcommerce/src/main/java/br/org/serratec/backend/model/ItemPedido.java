cf1771597f82fc95784b097689a9e6002d73c090
Residencia Note 5 <juliadonascimentosantos7@gmail.com>
2021-06-10 11:28:20 -0300

    tabelas

--- trabalhoEcommerce/src/main/java/br/org/serratec/backend/model/ItemPedido.java
+++ trabalhoEcommerce/src/main/java/br/org/serratec/backend/model/ItemPedido.java
@@ -1,5 +1,95 @@
 package br.org.serratec.backend.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ItemPedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_item_pedido")
+	private Long iditemPedido;
+
+	@Column(name = "preco_venda")
+	private Integer precoVenda;
+
+	@Column(name = "quantidade")
+	private Integer quantidade;
+
+	@Embedded
+	private Pedido pedido;
+
+	public ItemPedido() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemPedido(Long iditemPedido, Integer precoVenda, Integer quantidade, Pedido pedido) {
+		super();
+		this.iditemPedido = iditemPedido;
+		this.precoVenda = precoVenda;
+		this.quantidade = quantidade;
+		this.pedido = pedido;
+	}
+
+	public Long getIditemPedido() {
+		return iditemPedido;
+	}
+
+	public void setIditemPedido(Long iditemPedido) {
+		this.iditemPedido = iditemPedido;
+	}
+
+	public Integer getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(Integer precoVenda) {
+		this.precoVenda = precoVenda;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((iditemPedido == null) ? 0 : iditemPedido.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (iditemPedido == null) {
+			if (other.iditemPedido != null)
+				return false;
+		} else if (!iditemPedido.equals(other.iditemPedido))
+			return false;
+		return true;
+	}
 
 }
--- trabalhoEcommerce/src/main/java/br/org/serratec/backend/model/Pedido.java
+++ trabalhoEcommerce/src/main/java/br/org/serratec/backend/model/Pedido.java
@@ -1,5 +1,121 @@
 package br.org.serratec.backend.model;
 
-public class Pedido {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_pedido")
+	private Long idPedido;
+
+	@Column(name = "data_pedido")
+	private LocalDate dataPedido;
+
+	@Column(name = "id_entrega")
+	private LocalDate dataEntrega;
+
+	@Column(name = "data_envio")
+	private LocalDate dataEnvio;
+
+	@Column(name = "status")
+	private String status;
+
+	@Embedded
+	private Cliente cliente;
+
+	public Pedido() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pedido(Long idPedido, LocalDate dataPedido, LocalDate dataEntrega, LocalDate dataEnvio, String status,
+			Cliente cliente) {
+		super();
+		this.idPedido = idPedido;
+		this.dataPedido = dataPedido;
+		this.dataEntrega = dataEntrega;
+		this.dataEnvio = dataEnvio;
+		this.status = status;
+		this.cliente = cliente;
+	}
+
+	public Long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public LocalDate getDataEnvio() {
+		return dataEnvio;
+	}
+
+	public void setDataEnvio(LocalDate dataEnvio) {
+		this.dataEnvio = dataEnvio;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idPedido == null) ? 0 : idPedido.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (idPedido == null) {
+			if (other.idPedido != null)
+				return false;
+		} else if (!idPedido.equals(other.idPedido))
+			return false;
+		return true;
+	}
 }
